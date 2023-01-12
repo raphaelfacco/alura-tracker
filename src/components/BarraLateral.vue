0d9b37620ffad31943dec475d6455edb1e4afc05
raphaelfacco <raphaelfacco@mppa.mp.br>
2023-01-12 10:01:46 -0300

    Primeiro commit do projeto

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,54 @@
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logo.png" alt=""/>
+        </h1>
+        <button class="button" @click="alterarTema">
+          {{textoBotao}}
+        </button>
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'BarraLateral',
+    emits: ['aoTemaAlterado'],
+    data(){
+      return {
+        modoEscurotAtivo: false
+    }
+    },
+    computed: {
+      textoBotao(){
+        if(this.modoEscurotAtivo){
+          return 'Desativar modo escuro'
+        }
+        return 'Ativar modo escuro'
+      }
+    },
+    methods:{
+      alterarTema(){
+        this.modoEscurotAtivo = !this.modoEscurotAtivo;
+        this.$emit('aoTemaAlterado', this.modoEscurotAtivo);
+      }
+    }
+});
+</script>
+
+<style scoped>
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+}
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
---
+++ src/components/MeuBotao.vue
@@ -0,0 +1,28 @@
+<template>
+    <button class="button">
+        <span class="icon">
+            <i class="fas fa-play"></i>
+        </span>
+        <span>{{ titleBotao }}</span>
+    </button>
+    {{ estiloBotao }}
+</template>
+
+<script lang ="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'MeuBotao',
+    props: {
+        titleBotao: {
+            type: String,
+            default: ''
+        },
+        estiloBotao: {
+            type: String,
+            default: ''
+        }
+    },
+    emits: ['acaoAoClicar'],
+})   
+</script>
